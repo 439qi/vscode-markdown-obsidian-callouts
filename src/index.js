dfb13ce382c164bdd4b9d0a24933ef7836cd2426
Alice <al1s@gamedev.millennium>
2025-03-07 16:33:54 +0800

    convert to js

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import {
+  inspectBlockquoteContent,
+  inspectFencedCodeContent,
+  renderCalloutPostfix,
+  renderCalloutPrefix,
+} from "./inspect";
+
+// Define your plugin
+export default function mdItObsidianCallouts(md, options = {}) {
+  md.core.ruler.after("block", "obsidian-callouts", (state) => {
+    const tokens = state.tokens;
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+      if (token.type === "blockquote_open") {
+        inspectBlockquoteContent(tokens, i);
+      }
+      if (token.type === "fence") {
+        inspectFencedCodeContent(tokens, i, md, options);
+      }
+    }
+  });
+
+  md.renderer.rules.callout_open = (tokens, idx) => {
+    const token = tokens[idx];
+    return renderCalloutPrefix(token, md, options);
+  };
+
+  md.renderer.rules.admonition_block = (tokens, idx) => {
+    const token = tokens[idx];
+    return `${renderCalloutPrefix(token, md, options)}${
+      token.content
+    }\n</div>\n</div>`;
+  };
+
+  md.renderer.rules.callout_close = (tokens, idx) => {
+    const token = tokens[idx];
+    return renderCalloutPostfix(token, options);
+  };
+}
